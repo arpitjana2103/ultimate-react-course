3db34806b7d12b3ddb5fa227998b622a6ddc1a5d
Arpit Jana <arpitjana2103@gmail.com>
2025-12-11 18:59:07 +0530

    usePopcorn Layout done

---
+++ 07-usepopcorn/use-popcorn/src/components/MovieLength.jsx
@@ -0,0 +1,9 @@
+function MovieLength({ movies }) {
+    return (
+        <p className="num-results">
+            Found <strong>{movies.length}</strong> results
+        </p>
+    );
+}
+
+export default MovieLength;
